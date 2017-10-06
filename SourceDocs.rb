8a5a5818570827a44f4862582a8175ea6543058e
Eneko Alonso <eneko.alonso@gmail.com>
2017-10-06 13:49:27 -0700

    Release 0.2.0

--- SourceDocs.rb
+++ SourceDocs.rb
@@ -1,9 +1,9 @@
 class Sourcedocs < Formula
   desc "Command Line Tool written in Swift that generates Markdown files from inline source code documentation"
   homepage "https://github.com/eneko/SourceDocs"
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/eneko/SourceDocs/archive/#{version}.tar.gz"
-  sha256 "d908ce6d8fb6f61ac42b802a67b9f5a25b77fdfa21f3878bac4478b62dc4d8ff"
+  sha256 "086e5de5fe24c3a1da44751765628b5aef178b5a778c3a806455df7f38016978"
   head "https://github.com/eneko/SourceDocs.git"
 
   depends_on :xcode
