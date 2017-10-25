33533d812f08dbf14443619e554d7cce86632bb3
Eneko Alonso <eneko.alonso@gmail.com>
2017-10-25 09:13:21 -0700

    Release 0.4.0

--- SourceDocs.rb
+++ SourceDocs.rb
@@ -1,20 +1,21 @@
 class Sourcedocs < Formula
   desc "Command Line Tool written in Swift that generates Markdown files from inline source code documentation"
   homepage "https://github.com/eneko/SourceDocs"
-  version "0.3.0"
+  version "0.4.0"
   url "https://github.com/eneko/SourceDocs/archive/#{version}.tar.gz"
   sha256 "e57c8852ee7bbe8f1f4f85331972b4a87c5c8d62e621c52238763ed780732d66"
   head "https://github.com/eneko/SourceDocs.git"
 
   depends_on :xcode
 
-  # def install
-  #   system "make", "install", "PREFIX=#{prefix}"
-  # end
   def install
-    build_path = "#{buildpath}/.build/release/sourcedocs"
-    ohai "Building SourceDocs"
-    system("swift build --disable-sandbox -c release -Xswiftc -static-stdlib")
-    bin.install build_path
+    ohai "Installing SourceDocs..."
+    system "make", "install", "PREFIX=#{prefix}"
   end
+#   def install
+#     build_path = "#{buildpath}/.build/release/sourcedocs"
+#     ohai "Building SourceDocs"
+#     system("swift build --disable-sandbox -c release -Xswiftc -static-stdlib")
+#     bin.install build_path
+#   end
 end
