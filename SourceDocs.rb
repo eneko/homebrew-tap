1815961de20a9dbd44761c9b95a6111b160436a3
Eneko Alonso <eneko.alonso@gmail.com>
2017-10-25 09:15:18 -0700

    Update release sha

--- SourceDocs.rb
+++ SourceDocs.rb
@@ -3,7 +3,7 @@ class Sourcedocs < Formula
   homepage "https://github.com/eneko/SourceDocs"
   version "0.4.0"
   url "https://github.com/eneko/SourceDocs/archive/#{version}.tar.gz"
-  sha256 "e57c8852ee7bbe8f1f4f85331972b4a87c5c8d62e621c52238763ed780732d66"
+  sha256 "78db15b64700915489e2393715d9ff97ac574cbdf93bbf7daaeb92d371454498"
   head "https://github.com/eneko/SourceDocs.git"
 
   depends_on :xcode
