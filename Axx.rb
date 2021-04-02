ed2b2f0913b26adba32b2d917858d6d125c9e43c
Eneko Alonso <eneko.alonso@gmail.com>
2021-04-02 16:04:14 -0700

    Update Axx.rb

--- Axx.rb
+++ Axx.rb
@@ -10,7 +10,7 @@ class Axx < Formula
 
   def install
     build_path = "#{buildpath}/.build/release/axx"
-    system("swift build --disable-sandbox -c release -Xswiftc -static-stdlib")
+    system("swift build --disable-sandbox -c release")
     bin.install build_path
   end
 end
