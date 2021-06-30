19b2ad092711682c6824d494a3346e1bcb45276b
Eneko Alonso <eneko.alonso@gmail.com>
2021-06-30 13:14:07 -0700

    Update to 0.1.0

--- Axx.rb
+++ Axx.rb
@@ -1,9 +1,9 @@
 class Axx < Formula
   desc "Encrypt / decrypt files from the terminal"
   homepage "https://github.com/eneko/Axx"
-  version "0.0.2"
+  version "0.1.0"
   url "https://github.com/eneko/Axx/archive/#{version}.tar.gz"
-  sha256 "6289d56e4b9ff5bcb56a78bb8175d5c00786ec19e1ea7b930395857225b19490"
+  sha256 "b7ed391502c6a28edc4858b2fd0aa170ae255675c4fc05a903fab026563e0761"
   head "https://github.com/eneko/Axx.git"
 
   depends_on :xcode => ["10.0"]
